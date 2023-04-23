67c80b2ca7bfe961ef1d0c0d4f1a69fc93592227
eugenebatanov <evgeni.batanov@gmail.com>
2023-04-23 12:24:41 +0200

    done

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,9 @@
+import { save, load } from './storage.js';
+
+const STORAGE_KEY = 'tasks';
+
+let currentId = 0;
+
 const myInput = document.getElementById('myInput');
 const myUL = document.getElementById('myUL');
 
@@ -17,21 +23,63 @@ function addNewTask() {
     return;
   }
   createLi(task);
+  addTaskToStorage(task);
 }
 
-function createLi(text) {
+function createLi(text, isDone = false, id = currentId) {
   const liEl = document.createElement('li');
   liEl.textContent = text;
+  liEl.dataset.id = id;
+  if (isDone) liEl.classList.add('checked');
   myUL.appendChild(liEl);
   addCloseButton(liEl);
 }
 
 function handleTaskBehaviour({ target }) {
+  const currentState = load(STORAGE_KEY);
   if (target.nodeName === 'LI') {
     target.classList.toggle('checked');
+    const taskIndex = currentState.findIndex(
+      task => Number(task.id) === Number(target.dataset.id),
+    );
+    currentState[taskIndex].isDone = !currentState[taskIndex].isDone;
   } else if (target.classList.contains('close')) {
     target.parentNode.remove();
+    const taskIndex = currentState.findIndex(
+      task => Number(task.id) === Number(target.parentNode.dataset.id),
+    );
+    currentState.splice(taskIndex, 1);
+  }
+  save(STORAGE_KEY, currentState);
+}
+
+function createTaskObj(text, isDone) {
+  return {
+    text,
+    isDone,
+    id: currentId,
+  };
+}
+
+function addTaskToStorage(text, isDone = false) {
+  const currentState = load(STORAGE_KEY);
+  if (currentState === undefined) {
+    const arr = [createTaskObj(text, isDone)];
+    console.log(arr);
+    save(STORAGE_KEY, arr);
+  } else {
+    currentState.push(createTaskObj(text, isDone));
+    save(STORAGE_KEY, currentState);
+  }
+  currentId += 1;
+}
+
+function fillTasksList() {
+  const currentState = load(STORAGE_KEY);
+  if (currentState !== undefined) {
+    currentState.forEach(({ text, isDone, id }) => createLi(text, isDone, id));
+    currentId = id + 1;
   }
 }
 
-export { addNewTask, handleTaskBehaviour };
+export { addNewTask, handleTaskBehaviour, fillTasksList };
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-import { addNewTask, handleTaskBehaviour } from './functions.js';
+import { addNewTask, handleTaskBehaviour, fillTasksList } from './functions.js';
 
 const addBtn = document.getElementById('addBtn');
 const myUL = document.getElementById('myUL');
 
 addBtn.addEventListener('click', addNewTask);
 myUL.addEventListener('click', handleTaskBehaviour);
+window.addEventListener('DOMContentLoaded', fillTasksList);
